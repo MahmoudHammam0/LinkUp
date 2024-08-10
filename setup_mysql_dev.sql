d3911484ece4121b0feff908d85371326abdeff9
youssefelghamour <youssefelghamour@gmail.com>
2024-08-10 10:32:45 +0100

    add base_model.py and engine

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- setup database and user for LinkUp project
+-- how to use it: cat setup_mysql_dev.sql | mysql -hlocalhost -uroot -p
+CREATE DATABASE IF NOT EXISTS linkup_db;
+CREATE USER IF NOT EXISTS 'linkup_user'@'localhost' IDENTIFIED BY 'linkup_pwd';
+GRANT ALL PRIVILEGES ON artall_db.* TO 'linkup_user'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'linkup_user'@'localhost';
