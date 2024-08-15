54f185c9daca6b7f4141aeb6a9d0e34a8c00be2a
youssefelghamour <youssefelghamour@gmail.com>
2024-08-15 15:10:35 +0100

    Make the Feed Page more dynamic: Suggestions section

--- web_flask/static/scripts/feed.js
+++ web_flask/static/scripts/feed.js
@@ -40,4 +40,62 @@ $(document).ready(function() {
         });
     }
 
+    $.ajax({
+        url: 'http://localhost:5001/api/v1/users',
+        method: 'GET',
+        success: function(allUsers) {
+            // Fetch users the current user follows
+            $.ajax({
+                url: `http://localhost:5001/api/v1/users/${userId}/following`,
+                method: 'GET',
+                success: function(followingUsers) {
+                    // Create suggestions array
+                    const suggestions = [];
+    
+                    for (let i = 0; i < allUsers.length; i++) {
+                        let userIsFollowing = false;
+    
+                        // Check if current user is in the following list
+                        for (let j = 0; j < followingUsers.length; j++) {
+                            if (allUsers[i].id === followingUsers[j].id) {
+                                userIsFollowing = true;
+                                break;
+                            }
+                        }
+    
+                        // If the user isn't followed, add it to suggestions
+                        if (!userIsFollowing) {
+                            suggestions.push(allUsers[i]);
+                        }
+                    }
+    
+                    // Display suggestions
+                    const suggestionsList = $('.suggestions ul');
+                    suggestionsList.empty();
+                    for (let i = 0; i < suggestions.length; i++) {
+                        const user = suggestions[i];
+                        suggestionsList.append(`
+                            <li>
+                                <img src="../static/images/2.jpg">
+                                <span>${user.name} <h5>1 Mutual Friend</h5> </span>
+                                <button class="follow-button" data-user-id="${user.id}">Follow</button>
+                            </li>
+                        `);
+                    }
+    
+                    // Click event for follow button
+                    /*
+                    $('.follow-button').click(function() {
+                        const followUserId = $(this).data('user-id');
+                        $.ajax({
+                            url: `http://localhost:5001/api/v1/users/${userId}/follow/${followUserId}`,
+                            method: 'POST'
+                        });
+                    });
+                    */
+                }
+            });
+        }
+    });
+
 });
